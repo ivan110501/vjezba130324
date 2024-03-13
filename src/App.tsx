602d99f1323274aa41025105852d25f1bc826e34
ivan110501 <ivan.antunovic292@gmail.com>
2024-03-13 20:36:18 +0100

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Header from "./components/header";
 import Layout from "./components/layout";
 import "./styles/style.scss";
 function App() {
   return (
     <>
+      <Header />
       This is Git exercise
       <Layout />
     </>
