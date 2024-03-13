39daf36eb0e6ee0f3b4b6d524ea7bec6af0215fc
ivan110501 <ivan.antunovic292@gmail.com>
2024-03-13 20:28:44 +0100

    add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import Layout from "./components/layout";
 import "./styles/style.scss";
 function App() {
-  return <>This is Git exercise</>;
+  return (
+    <>
+      This is Git exercise
+      <Layout />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/layout.tsx
@@ -0,0 +1,9 @@
+const Layout = () => {
+  return (
+    <>
+      <div className="layout">Ovo je neki layout</div>
+    </>
+  );
+};
+
+export default Layout;
